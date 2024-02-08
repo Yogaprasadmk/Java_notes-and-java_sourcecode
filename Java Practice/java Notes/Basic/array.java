6a27b5bfdbdc1a1598a9a5ada76002773fdc3de2
Yogaprasad  M K <120255515+Yogaprasadmk@users.noreply.github.com>
2024-02-08 21:37:46 +0530

    Update array.java

--- Java Practice/java Notes/Basic/array.java
+++ Java Practice/java Notes/Basic/array.java
@@ -1,1 +1,13 @@
-y
+import java.util.Scanner;
+ class Main{
+     public static void main(String args[]){
+         Scanner ar = new Scanner(System.in);
+         int n = 5;
+         int a[] = new int[n];
+         for(int i = 0;i<n;i++){
+             a[i] = ar.nextInt();
+             System.out.print(a[i]+" ");
+         }
+         System.out.println();
+     }
+ }
