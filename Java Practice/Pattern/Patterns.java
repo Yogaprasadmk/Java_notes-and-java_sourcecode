5f5e2c19d0b08f406dccdd4a49b85cee21746338
Yogaprasad  M K <120255515+Yogaprasadmk@users.noreply.github.com>
2024-02-20 18:54:54 +0530

    Patterns.java

--- Java Practice/Pattern/Patterns.java
+++ Java Practice/Pattern/Patterns.java
@@ -479,6 +479,32 @@ class Patterns
          *                 * 
 
         */
+        //pattern 21
+        static void Pattern21(int n){
+            // outer loop
+            for(int i = 0;i<n;i++){
+                // inner loop
+                for(int j = 0;j<n;j++)
+                {
+                    if(i == 0 || i == n-1 || j == 0 || j == n-1){
+
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+        output:
+        /*
+        *****
+        *   *
+        *   *
+        *   *
+        *****
+        
         public static void main(String[] args) 
         {
                     int n = 5;
